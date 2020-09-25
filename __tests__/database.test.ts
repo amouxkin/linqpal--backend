98c3cec16184d6ab4f33c7867401c623e56f814e
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2020-09-26 04:31:16 +0530

    chore(database): Connection and disconnection to mongo configured.

--- __tests__/database.test.ts
+++ __tests__/database.test.ts
@@ -1,9 +1,13 @@
-import { connectToDatabase } from 'utilities/database';
+import { connectToDatabase, closeDatabase } from 'utilities/database';
 
 describe('Database connection', () => {
   it('should connect -- when the correct credentials are provided', async done => {
-    connectToDatabase.then(respose => {
+    connectToDatabase().then(() => {
       done();
     });
+  });
+
+  it('should disconnect -- when called after connection successful', async done => {
+    closeDatabase().then(() => done());
   });
 });
---
+++ src/utilities/database/index.ts
@@ -0,0 +1,19 @@
+import { connect, connection, disconnect } from 'mongoose';
+
+export const connectToDatabase = async () => {
+  connection
+    .on('error', error => console.log('Database connection error\n', error))
+    .once('open', () => console.log('Connected to database.\n'));
+
+  await connect(process.env.DATABASE_URL!, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }).catch(error => console.log('Database connection failure.\n', error));
+};
+
+export const closeDatabase = async () =>
+  disconnect()
+    .catch(error => console.log('Database disconnection error.\n', error))
+    .then(() => console.log('Database connection closed.m'));
