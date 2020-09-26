264b925e7e3810e6056e0f100532296243f1cf0c
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2020-09-27 00:46:38 +0530

    feat(auth): Password check.

--- __tests__/middlewares/authentication.test.ts
+++ __tests__/middlewares/authentication.test.ts
@@ -1,9 +1,17 @@
-import { createHashedPassword } from 'server/middlewares/authentication/hashing';
+import {
+  checkHashedPassword,
+  createHashedPassword
+} from 'server/middlewares/authentication/hashing';
 
 describe('Hashing', () => {
   it('should not be equal -- (even) when input is the same', () => {
     expect(createHashedPassword('Hello World')).not.toBe(
       createHashedPassword('Hello World')
     );
+  });
+
+  it('should be comparable -- when password is correct', () => {
+    const hashedPassword = createHashedPassword('hello world');
+    expect(checkHashedPassword('hello world', hashedPassword)).toBe(true);
   });
 });
--- src/server/middlewares/authentication/hashing.ts
+++ src/server/middlewares/authentication/hashing.ts
@@ -1,4 +1,11 @@
-import { hashSync } from 'bcrypt';
+import { hashSync, compareSync } from 'bcrypt';
 
 export const createHashedPassword = (password: string): string =>
   hashSync(password, parseInt(process.env.SALT_ROUNDS!));
+
+export const checkHashedPassword = (
+  password: string,
+  hashedPassword: string
+): boolean => {
+  return compareSync(password, hashedPassword);
+};
